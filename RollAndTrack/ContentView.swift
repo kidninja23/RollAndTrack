1b997184779cff025454dbff5286f68237f59998
B Team <jbice@bteam.games>
2020-01-15 14:14:39 -0500

    Initial Commit

---
+++ RollAndTrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RollAndTrack
+//
+//  Created by Jason Bice on 1/15/20.
+//  Copyright © 2020 B Team Games. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
